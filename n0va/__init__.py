f280c752dbd3a7a6f982df6c11bb66a380883d57
LobeliaComputing <31212444+ibuninngu@users.noreply.github.com>
2023-08-13 20:02:45 +0900

    Update __init__.py

--- n0va/__init__.py
+++ n0va/__init__.py
@@ -8,7 +8,8 @@ import n0va.util.cert as cert
 
 class Service(http.server):
     def EnableSSL(self, domain_cert, private_key):
-        ctx = ssl.create_default_context()
+        ctx = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        ctx.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
         ctx.load_cert_chain(domain_cert, private_key)
         self._SSL_Context = ctx
 
